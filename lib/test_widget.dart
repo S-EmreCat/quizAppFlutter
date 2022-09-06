62e0ddf9ccdd367d22f024a00c638f090cca1014
S-EmreCat <emre__cat@outlook.com>
2022-09-06 09:45:10 +0300

    first commit

---
+++ lib/test_widget.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class TestWidget extends StatefulWidget {
+  const TestWidget({super.key});
+
+  @override
+  State<TestWidget> createState() => _TestWidgetState();
+}
+
+class _TestWidgetState extends State<TestWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("test widget page"),
+      ),
+      body: const Center(),
+    );
+  }
+}
